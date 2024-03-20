d65398b80b73f1258eec2dfc2174dc328a14843c
Grzechu700 <157650272+Grzechu700@users.noreply.github.com>
2024-03-20 21:19:57 +0100

    Add files via upload

---
+++ 1 zadanie.py
@@ -0,0 +1,8 @@
+title = "Zadanie"
+print("Tytuł maila: " + title)
+
+contents = "Moje pierwsze zadanie w języku Python"
+print("Tresć maila: " + contents)
+
+sender = "Marcin"
+print("Nadawca maila: " + sender + " Grzechnik")
